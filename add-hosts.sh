8b52319768bdd22ff5322471b057a3dcfb724727
Howie <josephwilliamdoctor@gmail.com>
2022-09-29 18:43:24 +0800

    Initial commit

---
+++ add-hosts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./manage-etc-hosts.sh add monitor.josephdomain.com
+./manage-etc-hosts.sh add db-admin.josephdomain.com
+./manage-etc-hosts.sh add blog.josephdomain.com
---
+++ remove-hosts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./manage-etc-hosts.sh remove monitor.josephdomain.com
+./manage-etc-hosts.sh remove db-admin.josephdomain.com
+./manage-etc-hosts.sh remove blog.josephdomain.com
